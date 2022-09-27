6f66feafb3cd4b314ef2cd218cacfdc74b826310
kryde507 <92607528+kryde507@users.noreply.github.com>
2022-09-27 15:00:54 -0600

    JS functionality and Responsive design
    Added the JS functionality for submitting a rating. Adjusted the design to be responsive

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const submitForm = document.querySelector("#submit");
 const mainCard = document.querySelector("#main-card");
 const secondaryCard = document.querySelector("#secondary-card");
 const ratingForm = document.querySelector("#rating");
-let selectedRating = document.querySelector("input[name='rating']:checked").value;
+
 
 
 ratingForm.addEventListener('submit', function (e) {
-    e.preventDefault;
+    e.preventDefault();
     mainCard.classList.add('visually-hidden');
     secondaryCard.classList.remove('visually-hidden');
+    let selectedRating = document.querySelector("input[name='btnradio']:checked").value;
     let finalRating = document.querySelector("#final-rating");
     finalRating.innerText = selectedRating;
     console.log(selectedRating);
