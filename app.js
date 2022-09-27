e5cf70285ea74bbe938cf04c66f9725b7dfd9ea2
kryde507 <92607528+kryde507@users.noreply.github.com>
2022-09-27 08:39:00 -0600

    Fonts and grid
    Installed fonts and added some grid layouts

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const submitForm = document.querySelector("#submit");
 const mainCard = document.querySelector("#main-card");
 const secondaryCard = document.querySelector("#secondary-card");
 
-
+let selectedRating = document.querySelector("input[name='rating']:checked").value;
 
 submitForm.addEventListener('click', function () {
     mainCard.classList.add('visually-hidden');
     secondaryCard.classList.remove('visually-hidden');
-    let selectedRating = document.querySelector("input[name='rating']:checked").value;
+
     console.log(selectedRating);
 })
 
-
-// rating.innerText = selectedRating
+let finalRating = document.querySelector("#final-rating");
+finalRating.innerText = selectedRating;
